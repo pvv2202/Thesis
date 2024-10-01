b697f067a99cfb9bdd2b74d94ea1fa0a949ac003
pvv2202 <paul.vandervort2@gmail.com>
2024-10-01 19:31:52 -0400

    Updated a lot fo small things to make different layers compatible with others

--- Interpreter.py
+++ Interpreter.py
@@ -1,21 +1,30 @@
 import keras
 from keras import layers
 
+#TODO: ADD STRIDE ABILITY, IMPLEMENT GLOBAL POOLING IN A WAY THAT IT DOESN'T JUST BREAK EVERY MODEL THAT HAS IT
+
+DIM = 32
+CHANNELS = 5
+KERNEL = 3
+POOL = 2
+DROPOUT = 0.5
+ACTIVATION = 'relu'
+OPTIMIZER = 'adam'
+LOSS = 'categorical_crossentropy'
+METRICS = 'accuracy'
+START_LAYER = [28, 28, 1]
+
+valid_instructions = {'dense', 'conv', 'dropout', 'normalize', 'global_pool', 'max_pool', 'compile', 'input_layer', 'output_layer', 'fit', 'evaluate'}  # Set of all valid code types
+valid_activations = {'relu', 'sigmoid', 'softmax'}  # Set of all valid activations
+valid_loss = {'sparse_categorical_crossentropy', 'binary_crossentropy', 'categorical_crossentropy'}  # Set of all valid losses
+valid_optimizers = {'adam', 'sgd', 'rmsprop'}  # Set of all valid optimizers
+valid_metrics = {'accuracy', 'precision', 'recall'}  # Set of all valid metrics
+
+valid_instruction_mutations = ['dense', 'conv', 'dropout', 'normalize', 'global_pool', 'max_pool']  # List of all valid code types
+valid_activation_mutations = ['relu', 'sigmoid', 'softmax']
+
 class PushInterpreter():
     '''Interprets and processed Push programs'''
-    DIM = 32
-    KERNEL = 3
-    DROPOUT = 0.5
-    ACTIVATION = 'relu'
-    OPTIMIZER = 'adam'
-    LOSS = 'categorical_crossentropy'
-    METRICS = 'accuracy'
-
-    valid_instructions = {'dense', 'conv', 'dropout', 'normalize', 'global_pool', 'max_pool', 'compile', 'input_layer', 'output_layer', 'fit'} # Set of all valid code types
-    valid_activations = {'relu', 'sigmoid', 'softmax'} # Set of all valid activations
-    valid_loss = {'sparse_categorical_crossentropy', 'binary_crossentropy', 'categorical_crossentropy'} # Set of all valid losses
-    valid_optimizers = {'adam', 'sgd', 'rmsprop'} # Set of all valid optimizers
-    valid_metrics = {'accuracy', 'precision', 'recall'} # Set of all valid metrics
 
     #TODO: Think about model fingerprint to prevent the same model from being tested. It is likely that mutations will have no change
 
@@ -33,6 +42,9 @@ class PushInterpreter():
         self.X_test = X_test
         self.y_test = y_test
 
+        # Dimensions of the output of the previous layer
+        self.previous_layer = START_LAYER
+
         # Initialize the model
         self.model = keras.Sequential()
 
@@ -47,7 +59,8 @@ class PushInterpreter():
             'compile': self.compile,
             'input_layer': self.input_layer,
             'output_layer': self.output_layer,
-            'fit': self.fit
+            'fit': self.fit,
+            'evaluate': self.evaluate
         }
 
     def read_genome(self, genome):
@@ -66,30 +79,29 @@ class PushInterpreter():
                 else:
                     self.str_stack.append(instruction)
 
-
     def pop_string(self, type=None):
         '''Pops a string from the stack'''
         if len(self.str_stack) == 0:
             if type == 'activation':
-                return self.ACTIVATION
+                return ACTIVATION
             elif type == 'optimizer':
-                return self.OPTIMIZER
+                return OPTIMIZER
             elif type == 'loss':
-                return self.LOSS
+                return LOSS
             elif type == 'metrics':
-                return self.METRICS
+                return METRICS
             else:
                 return ''
         else:
             string = self.str_stack.pop()
-            if type == 'activation' and string not in self.valid_activations:
-                return self.ACTIVATION
-            elif type == 'optimizer' and string not in self.valid_optimizers:
-                return self.OPTIMIZER
-            elif type == 'loss' and string not in self.valid_loss:
-                return self.LOSS
-            elif type == 'metrics' and string not in self.valid_metrics:
-                return self.METRICS
+            if type == 'activation' and string not in valid_activations:
+                return ACTIVATION
+            elif type == 'optimizer' and string not in valid_optimizers:
+                return OPTIMIZER
+            elif type == 'loss' and string not in valid_loss:
+                return LOSS
+            elif type == 'metrics' and string not in valid_metrics:
+                return METRICS
             else:
                 return string
 
@@ -98,9 +110,13 @@ class PushInterpreter():
         '''Pops an int from the stack'''
         if len(self.int_stack) == 0:
             if type == 'dim':
-                return self.DIM
+                return DIM
             elif type == 'kernel':
-                return self.KERNEL
+                return KERNEL
+            elif type == 'pool':
+                return POOL
+            elif type == 'channels':
+                return CHANNELS
             else:
                 return 0
         return self.int_stack.pop()
@@ -109,26 +125,57 @@ class PushInterpreter():
         '''Pops a float from the stack'''
         if len(self.float_stack) == 0:
             if type == 'dropout':
-                return self.DROPOUT
+                return DROPOUT
             else:
                 return 0.0
         return self.float_stack.pop()
 
-    """TENSORFLOW FUNCTIONS"""
+    """HELPER FUNCTIONS"""
+    def reshape_dense(self, channels):
+        '''Reshapes the previous layer to a 3D tensor'''
+        dim = self.pop_int('dim')
+        reshape_target = (dim, dim, channels)
+        # If previous layer is not the correct size, add a dense layer to reshape
+        if self.previous_layer[0] != dim * dim * channels:
+            activation = self.pop_string('activation')
+            self.model.add(layers.Dense(dim * dim * channels, activation=activation))
+        # Reshape layer into 2D tensor
+        self.model.add(layers.Reshape(reshape_target))
+        self.previous_layer = reshape_target
+
+    """GENETIC FUNCTIONS"""
     def dense(self):
         '''Adds a dense layer to the model'''
         dim = self.pop_int('dim')
         activation = self.pop_string('activation')
 
+        if len(self.previous_layer) > 1:
+            self.model.add(layers.Flatten())
+
         self.model.add(layers.Dense(dim, activation=activation))
+        self.previous_layer = [dim]
 
     def conv(self):
         '''Adds a convolutional layer to the model'''
-        dim = self.pop_int('dim')
+        channels = self.pop_int('channels')
         activation = self.pop_string('activation')
         k = self.pop_int('kernel')
+        print(k)
+        print(self.previous_layer[0])
 
-        self.model.add(layers.Conv2D(dim, kernel_size=(k, k), activation=activation))
+        # If previous layer is 1D, reshape it to 2D
+        if len(self.previous_layer) == 1:
+            self.reshape_dense(channels)
+
+        # If k > previous dimension, do nothing
+        if k > self.previous_layer[0]:
+            return
+
+        self.model.add(layers.Conv2D(channels, kernel_size=(k, k), activation=activation))
+
+        # Calculate new dimensions, assuming stride of 1
+        new_dim = self.previous_layer[0] - k + 1
+        self.previous_layer = [new_dim, new_dim, channels]
 
     def dropout(self):
         '''Adds a dropout layer to the model'''
@@ -142,11 +189,29 @@ class PushInterpreter():
 
     def global_pool(self):
         '''Adds a global pooling layer to the model'''
-        self.model.add(layers.GlobalAveragePooling2D())
+        pass
+        # if len(self.previous_layer) == 1:
+        #     return
+        #
+        # self.model.add(layers.GlobalAveragePooling2D()
+        #
+        # self.previous_layer = ()
 
     def max_pool(self):
         '''Adds a max pooling layer to the model'''
-        self.model.add(layers.MaxPool2D())
+        if len(self.previous_layer) == 1:
+            return
+
+        # Get pool size, add layer
+        p = self.pop_int('pool')
+        if p > self.previous_layer[0]:
+            return
+
+        self.model.add(layers.MaxPool2D(pool_size=(p, p)))
+
+        # Calculate new dimensions
+        new_dim = (self.previous_layer[0] - p) // p + 1
+        self.previous_layer = [new_dim, new_dim, 1]
 
     def compile(self):
         '''Compiles the model'''
@@ -160,27 +225,38 @@ class PushInterpreter():
 
     def input_layer(self):
         '''Adds an input layer to the model. This should be predefined'''
-        self.model.add(keras.Input(shape=(28, 28, 1)))
+        self.model.add(keras.Input(shape=START_LAYER))
 
     def output_layer(self):
         '''Adds an output layer to the model. This should be predefined'''
-        self.dense()
+        # If previous layer is multidimensional, flatten it
+        if len(self.previous_layer) > 1:
+            self.model.add(keras.layers.Flatten())
+
         self.model.add(keras.layers.Dense(10, activation='softmax'))
 
     def fit(self):
         '''Fits the model to the data'''
-        self.model.fit(self.X_train, self.y_train, epochs=5)
+        self.model.fit(self.X_train, self.y_train, epochs=1)
 
     def evaluate(self):
         '''Evaluates the model on the test data'''
-        self.model.evaluate(self.X_test, self.y_test)
+        score = self.model.evaluate(self.X_test, self.y_test, verbose=0)
+        return score
 
     def run(self):
         '''Runs the interpreter'''
         while self.exec_stack:
             instruction = self.exec_stack.pop(0)
-            print(instruction)
             if callable(instruction):
-                instruction()
+                if instruction == self.evaluate:
+                    return instruction()
+                elif instruction == self.compile:
+                    instruction()
+                    if self.model.count_params() > 1000000:
+                        return [0]
+                else:
+                    instruction()
+
             else:
                 raise ValueError(f'Invalid instruction: {instruction}')
--- gp.py
+++ gp.py
@@ -1,32 +1,69 @@
 import Interpreter
 import random
 
+INT_RANGE = (1, 112)
+FLOAT_RANGE = (0.0, 1.0)
+
 class Genome():
     '''Genome of a Neural Network'''
-    INT_RANGE = [1, 200]
-    FLOAT_RANGE = [0.0, 1.0]
 
     def __init__(self):
-        self.genome = ['input_layer', 'output_layer', 'compile', 'fit']
+        self.genes = ['input_layer', 'output_layer', 'compile', 'fit', 'evaluate']
         self.fitness = 0
+
+    def add_gene(self):
+        '''Adds a gene'''
+        # Randomly select an index to mutate
+        index = random.randint(1, len(self.genes) - 4)
+        # Randomly select what type of thing to add
+        type = random.randint(0, 3)
+
+        match type:
+            case 0:
+                self.genes.insert(index, random.randint(*INT_RANGE))
+            case 1:
+                self.genes.insert(index, random.uniform(*FLOAT_RANGE))
+            case 2:
+                self.genes.insert(index, random.choice(Interpreter.valid_instruction_mutations))
+            case 3:
+                self.genes.insert(index, random.choice(Interpreter.valid_activation_mutations))
+
+    def remove_gene(self):
+        '''Removes a gene'''
+        # Randomly select an index to mutate
+        index = random.randint(1, len(self.genes) - 3)
+
+        # Check that the current index is not a required gene
+        if self.genes[index] not in {'input_layer', 'output_layer', 'compile', 'fit'}:
+            self.genes.pop(index)
 
     def mutate(self):
         '''Mutates the genome'''
-
         # Randomly select an index to mutate
-        index = random.randint(1, len(self.genome) - 3)
+        index = random.randint(1, len(self.genes) - 3)
 
-        # Randomly select what type of thing to add
+        # Check that the current index is not a required gene. If it is just return
+        if self.genes[index] in {'input_layer', 'output_layer', 'compile', 'fit'}:
+            return
+
+        # Randomly select what type of thing to mutate
         type = random.randint(0, 3)
         match type:
             case 0:
-                self.genome.insert(index, random.randint(*INT_RANGE))
+                self.genes[index] = random.randint(*INT_RANGE)
             case 1:
-                self.genome.insert(index, 'dense')
+                self.genes[index] = random.uniform(*FLOAT_RANGE)
             case 2:
-                self.genome.insert(index, 'dropout')
+                self.genes[index] = random.choice(Interpreter.valid_instruction_mutations)
             case 3:
-                self.genome.insert(index, 'normalize')
+                self.genes[index] = random.choice(Interpreter.valid_activation_mutations)
+
+    def transcribe(self, X_train, y_train, X_test, y_test):
+        '''Transcribes the genome'''
+        interpreter = Interpreter.PushInterpreter(X_train, y_train, X_test, y_test)
+        interpreter.read_genome(self.genes)
+        score = interpreter.run()
+        self.fitness = score[0]
 
 class Population():
     '''Population of Neural Networks'''
@@ -35,31 +72,43 @@ class Population():
         self.fitness = [] # Each index corresponds to the fitness of the network at that index
         self.size = size
 
-    def initialize_population(self):
+    def initialize_population(self, gene_number=10):
         '''Initialize the population with random neural networks'''
         for i in range(self.size):
             self.population.append(Genome())
+            # Add 10 initial genes
+            for _ in range(gene_number):
+                self.population[i].add_gene()
 
-if __name__ == '__main__':
-    interpreter = Interpreter.PushInterpreter()
-    interpreter.exec_stack.extend([
-        interpreter.input_layer,
-        interpreter.conv,
-        interpreter.conv,
-        interpreter.max_pool,
-        interpreter.normalize,
-        interpreter.conv,
-        interpreter.max_pool,
-        interpreter.normalize,
-        interpreter.global_pool,
-        interpreter.output_layer,
-        interpreter.compile,
-        interpreter.fit
-    ])
-    interpreter.str_stack.extend([
-        'relu',
-        'relu',
-        'relu',
-        'relu',
-    ])
-    interpreter.run()
+    def forward_generation(self):
+        '''Move forward one generation'''
+        # Remove the bottom 3 from the population
+        self.population.sort(key=lambda x: x.fitness)
+
+        # Clone best 3
+        self.population[-3:] = self.population[:3]
+
+        # Mutate the rest. Add a gene with 35% probability, remove a gene with 15% probability, mutate a gene with 50% probability
+        for genome in self.population:
+            rand = random.random()
+            num = random.randint(1, 3)
+            if rand < 0.35:
+                for _ in range(num):
+                    genome.add_gene()
+            elif rand < 0.5:
+                for _ in range(num):
+                    genome.remove_gene()
+            else:
+                for _ in range(num):
+                    genome.mutate()
+
+    def run(self, X_train, y_train, X_test, y_test, generations=100):
+        '''Runs the genetic algorithm'''
+        self.initialize_population()
+        for _ in range(generations):
+            for genome in self.population:
+                genome.transcribe(X_train, y_train, X_test, y_test)
+            self.forward_generation()
+            print(self.population[0].fitness)
+            print(self.population[0].genes)
+            print()
